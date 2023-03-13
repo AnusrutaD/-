ede5a1044a7fa8a5153a4e12340e11f3c9d59268
Anusruta Dutta <Anusruta.Dutta@wisetechglobal.com>
2023-03-13 11:18:27 +0530

    My Personal Portfolio Basic Structure

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".home-content h3 span", {
+    strings:["Fullstack Developer", "Software Engineer", "Coder"],
+    typeSpeed: 100,
+    backSpace:100,
+    backDelay:1000,
+    loop: true
+})
